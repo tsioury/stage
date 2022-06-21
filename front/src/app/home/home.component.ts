befbd2b2c40b54b7e82f802275ab09ffab950a09
Tsiory Ramarolahy <tramarolah@gmail.com>
2022-06-21 15:08:51 +0300

    aller directement dans home sans s'authentifier impossible

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  
+  authStatus!: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.authStatus = this.authService.isAuth;
+    console.log(this.authStatus);
+    if (!this.authStatus) {
+      this.router.navigate(['auth']);
+    }
   }
-
 }
